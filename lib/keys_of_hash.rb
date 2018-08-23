88c3f6713d82bb88fb79298c355f38427bdd5bde
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 13:21:03 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pp' 
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    a = []
+    self.each do |key, val|
+      arguments.each do |arg|
+        a << key if val == arg 
+      end 
+    end
+    a
   end
 end
